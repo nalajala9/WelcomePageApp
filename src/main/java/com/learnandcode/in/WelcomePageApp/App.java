6da3c56034257d0f61062e577dd3daa50669796c
ravikumar <nalajalaravi99@gmail.com>
2022-10-26 09:11:18 +0530

    adding sourcefile and pom.xml

---
+++ src/main/java/com/learnandcode/in/WelcomePageApp/App.java
@@ -0,0 +1,13 @@
+package com.learnandcode.in.WelcomePageApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to LearnandCode.in" );
+    }
+}
